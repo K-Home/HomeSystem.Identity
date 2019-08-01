fdf641d36e06d4c9e1b58e843ec1b968d5e8e2df
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2019-08-01 13:27:59 +0200

    Added generic pagination classes

---
+++ src/HomeSystem.Services.Identity.Infrastructure/Pagination/Pagination.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
+using HomeSystem.Services.Identity.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeSystem.Services.Identity.Infrastructure.Pagination
+{
+    public static class Pagination
+    {
+        public static async Task<PagedResults<TReturn>> PaginateAsync<T, TReturn>(
+            IQueryable<T> queryable,
+            int page,
+            int pageSize,
+            string orderBy,
+            bool ascending)
+        {
+            var skipAmount = pageSize * (page - 1);
+
+            var projection = queryable
+                .OrderByPropertyOrField(orderBy, ascending)
+                .Skip(skipAmount)
+                .Take(pageSize)
+                .ProjectTo<TReturn>();
+
+            var totalNumberOfRecords = await queryable.CountAsync();
+            var results = await projection.ToListAsync();
+            var mod = totalNumberOfRecords % pageSize;
+            var totalPageCount = totalNumberOfRecords / pageSize + (mod == 0 ? 0 : 1);
+
+            return new PagedResults<TReturn>
+            {
+                Results = results,
+                PageNumber = page,
+                PageSize = results.Count,
+                TotalNumberOfPages = totalPageCount,
+                TotalNumberOfRecords = totalNumberOfRecords,
+            };
+        }
+    }
+}
