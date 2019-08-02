e7615116e67e2dd53cc8db30e79ecbeadbf12f18
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2019-08-02 12:13:45 +0200

    Added MediatR behaviours.

---
+++ src/HomeSystem.Services.Identity.Application/Behaviors/LoggingBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using HomeSystem.Services.Identity.Infrastructure.Extensions;
+using MediatR;
+using Serilog;
+
+namespace HomeSystem.Services.Identity.Application.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private static readonly ILogger Logger = Log.Logger;
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            Logger.Information("----- Handling command {CommandName} ({@Command})", request.GetGenericTypeName(), request);
+            var response = await next();
+            Logger.Information("----- Command {CommandName} handled - response: {@Response}", request.GetGenericTypeName(), response);
+
+            return response;
+        }
+    }
+}
---
+++ src/HomeSystem.Services.Identity.Application/Behaviors/TransactionBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HomeSystem.Services.Identity.Infrastructure.EF;
+using HomeSystem.Services.Identity.Infrastructure.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
+
+namespace HomeSystem.Services.Identity.Application.Behaviors
+{
+    public class TransactionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TransactionBehaviour<TRequest, TResponse>> _logger;
+        private readonly IdentityDbContext _dbContext;
+
+        public TransactionBehaviour(IdentityDbContext dbContext,
+            ILogger<TransactionBehaviour<TRequest, TResponse>> logger)
+        {
+            _dbContext = dbContext ?? throw new ArgumentException(nameof(IdentityDbContext));
+            _logger = logger ?? throw new ArgumentException(nameof(ILogger));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var response = default(TResponse);
+            var typeName = request.GetGenericTypeName();
+
+            try
+            {
+                if (_dbContext.HasActiveTransaction)
+                {
+                    return await next();
+                }
+
+                var strategy = _dbContext.Database.CreateExecutionStrategy();
+
+                await strategy.ExecuteAsync(async () =>
+                {
+                    using (var transaction = await _dbContext.BeginTransactionAsync())
+
+                    using (LogContext.PushProperty("TransactionContext", transaction.TransactionId))
+                    {
+                        _logger.LogInformation("----- Begin transaction {TransactionId} for {CommandName} ({@Command})", transaction.TransactionId, typeName, request);
+
+                        response = await next();
+
+                        _logger.LogInformation("----- Commit transaction {TransactionId} for {CommandName}", transaction.TransactionId, typeName);
+
+                        await _dbContext.CommitTransactionAsync(transaction);
+                    }
+                });
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ERROR Handling transaction for {CommandName} ({@Command})", typeName, request);
+
+                throw;
+            }
+        }
+    }
+}
