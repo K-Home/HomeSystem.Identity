47a5c40ec7a1a8946d7235e36a62954ca837c5c7
pkantorowicz <piotrkantorowicz@gmail.com>
2019-10-04 17:14:41 +0200

    Improved a paged result, added filter and paged queries.

---
+++ src/FinanceControl.Services.Users.Infrastructure/Messages/IFIlter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FinanceControl.Services.Users.Infrastructure.Messages
+{
+    public interface IFilter<TResult, in TQuery> where TQuery : IQuery
+    {
+        IEnumerable<TResult> Filter(IEnumerable<TResult> values, TQuery query);
+    }
+}
--- src/FinanceControl.Services.Users.Infrastructure/Pagination/PagedResult.cs
+++ src/FinanceControl.Services.Users.Infrastructure/Pagination/PagedResult.cs
@@ -1,32 +1,52 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace FinanceControl.Services.Users.Infrastructure.Pagination
 {
     public class PagedResult<T>
     {
-        /// <summary>
-        /// The page number this page represents. 
-        /// </summary>
-        public int PageNumber { get; set; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+        public int TotalNumberOfPages { get; }
+        public int TotalNumberOfRecords { get; }
+        public IEnumerable<T> Results { get; }
+        
+        public bool IsEmpty => Results == null || !Results.Any();
+        public bool IsNotEmpty => !IsEmpty;
 
-        /// <summary> 
-        /// The size of this page. 
-        /// </summary> 
-        public int PageSize { get; set; }
+        protected PagedResult()
+        {
+            Results = Enumerable.Empty<T>();
+        }
 
-        /// <summary> 
-        /// The total number of pages available. 
-        /// </summary> 
-        public int TotalNumberOfPages { get; set; }
+        protected PagedResult(int pageNumber, int pageSize,
+            int totalNumberOfPages, int totalNumberOfRecords,
+            IEnumerable<T> results)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalNumberOfPages = totalNumberOfPages;
+            TotalNumberOfRecords = totalNumberOfRecords;
+            Results = results;
+        }
 
-        /// <summary> 
-        /// The total number of records available. 
-        /// </summary> 
-        public int TotalNumberOfRecords { get; set; }
+        public static PagedResult<T> Create(int pageNumber, int pageSize, 
+            int totalNumberOfPages, int totalNumberOfRecords, IEnumerable<T> results)
+        {
+            var pagedResult =
+                new PagedResult<T>(pageNumber, pageSize, totalNumberOfPages, totalNumberOfRecords, results);
 
-        /// <summary> 
-        /// The records this page represents. 
-        /// </summary> 
-        public IEnumerable<T> Results { get; set; }
+            return pagedResult;
+        }
+
+        public static PagedResult<T> From(PagedResult<T> result, IEnumerable<T> items)
+        {
+            var pagedResult = new PagedResult<T>(result.PageNumber, result.PageSize,
+                result.TotalNumberOfPages, result.TotalNumberOfRecords, items);
+
+            return pagedResult;
+        }
+
+        public static PagedResult<T> Empty => new PagedResult<T>();
     }
 }
