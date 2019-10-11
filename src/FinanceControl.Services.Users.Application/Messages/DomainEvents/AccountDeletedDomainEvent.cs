bee186ca46002599a4369b231b69ee6cfe9a4c63
pkantorowicz <piotrkantorowicz@gmail.com>
2019-10-11 19:48:18 +0200

    Added classes for delete account request.

--- src/FinanceControl.Services.Users.Application/Messages/DomainEvents/AccountDeletedDomainEvent.cs
+++ src/FinanceControl.Services.Users.Application/Messages/DomainEvents/AccountDeletedDomainEvent.cs
@@ -1,9 +1,19 @@
-﻿using FinanceControl.Services.Users.Infrastructure.Messages;
+﻿using System;
+using FinanceControl.Services.Users.Infrastructure.Messages;
 
 namespace FinanceControl.Services.Users.Application.Messages.DomainEvents
 {
     public class AccountDeletedDomainEvent : IDomainEvent
     {
-        
+        public Guid RequestId { get; }
+        public Guid UserId { get;  }
+        public bool Soft { get;  }
+
+        public AccountDeletedDomainEvent(Guid requestId, Guid userId, bool soft)
+        {
+            RequestId = requestId;
+            UserId = userId;
+            Soft = soft;
+        }
     }
 }
