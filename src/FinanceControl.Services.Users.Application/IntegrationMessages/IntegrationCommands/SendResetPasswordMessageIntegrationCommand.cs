dca0a4420402dcbd9a2b47a3e58853bcc3514760
pkantorowicz <piotrkantorowicz@gmail.com>
2019-10-11 17:56:16 +0200

    Add change username request part.

---
+++ src/FinanceControl.Services.Users.Application/IntegrationMessages/IntegrationCommands/SendResetPasswordMessageIntegrationCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceControl.IntegrationMessages
+{
+    // namespace must be the same in services, required by MassTransit library
+    // https://stackoverflow.com/questions/52477283/masstransit-consume-equal-objects-defined-in-different-namespaces
+    // ReSharper disable once CheckNamespace
+    public class SendResetPasswordMessageIntegrationCommand
+    {
+    }
+}
